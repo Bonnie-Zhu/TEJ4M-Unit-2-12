a477e5120e47c5fc7897ed6f3db25a2f3249745b
Bonnie Zhu <bonnie.zhu@mths.ca>
2025-05-05 09:49:52 -0400

    finish 2-12

--- printf01.s
+++ printf01.s
@@ -1,6 +1,27 @@
+/* printf01.s - Fixed version */
 .data
-return: .word 0          @ 4 bytes (naturally aligned)
-number_read: .word 0     @ Next 4 bytes (aligned)
-message1: .asciz "Hey, type a number: "  @ 20 bytes (aligned after .word)
-message2: .asciz "I read the number %d\n" @ 20 bytes (aligned)
-scan_pattern: .asciz "%d"  @ 3 bytes (no alignment needed afterward)
+return: .word 0
+number_read: .word 0
+message1: .asciz "Hey, type a number: "
+message2: .asciz "I read the number %d\n"
+scan_pattern: .asciz "%d"
+
+.text
+.global main    @ Critical: Export main for the linker
+main:
+    ldr r1, =return
+    str lr, [r1]          @ Save return address
+    ldr r0, =message1
+    bl printf             @ Print "Hey, type a number: "
+    ldr r0, =scan_pattern
+    ldr r1, =number_read
+    bl scanf              @ Read integer into number_read
+    ldr r0, =message2
+    ldr r1, =number_read
+    ldr r1, [r1]         @ Load value of number_read
+    bl printf             @ Print "I read the number X"
+    ldr r1, =return
+    ldr lr, [r1]         @ Restore lr
+    bx lr                 @ Return
+
+.global printf, scanf  @ Declare external C functions
